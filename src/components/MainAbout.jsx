02bf1a2fc5a53becd44b19a14f18ac59adcbbf63
juanmartinmn <juan.molinanavarro@alu.frt.utn.edu.ar>
2025-06-09 10:45:43 -0300

    Utilizacion de JSON y useEffect en AboutMe

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -1,96 +1,50 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
+  const [datos, setDatos] = useState([]);
+
+  // Se ejecuta solo UNA VEZ cuando el componente se monta
+  useEffect(() => {
+    console.log("Cargando datos del JSON...");
+    fetch('../data/about.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setDatos(data);
+        console.log("Datos cargados");
+      })
+      .catch((error) => console.error("Error cargando JSON:", error));
+  }, []); // <--- Este array vacío hace que se ejecute solo una vez
+
+
+
 const MainAbout = () => {
     return (
         <div className='mainAbout'>
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>4 Hobbies:</Card.Title>
-                    <Card.Text>
-                        <ul>
-                            <li>
-                                Tocar la guitarra
-                            </li>
-                            <li>
-                                Leer
-                            </li>
-                            <li>
-                                Mirar peliculas
-                            </li>
-                            <li>
-                                Jugar videojuegos
-                            </li>
-                        </ul>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>4 Bandas:</Card.Title>
-                    <Card.Text>
-                        <ul>
-                            <li>
-                                Oasis
-                            </li>
-                            <li>
-                                The Rolling Stones
-                            </li>
-                            <li>
-                                Intoxicados
-                            </li>
-                            <li>
-                                Soda Stereo
-                            </li>
-                        </ul>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {datos.map((d) => (
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>4 Peliculas:</Card.Title>
-                    <Card.Text>
-                        <ul>
-                            <li>
-                                Bastardos Sin Gloria
-                            </li>
-                            <li>
-                                El Caballero de la Noche
-                            </li>
-                            <li>
-                                La La Land
-                            </li>
-                            <li>
-                                Anora
-                            </li>
-                        </ul>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>4 {d.titulo}:</Card.Title>
+                        <Card.Text>
+                            <ul>
+                                <li>
+                                    {d.item1}
+                                </li>
+                                <li>
+                                    {d.item2}
+                                </li>
+                                <li>
+                                    {d.item3}
+                                </li>
+                                <li>
+                                    {d.item4}
+                                </li>
+                            </ul>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>4 Videojuegos:</Card.Title>
-                    <Card.Text>
-                        <ul>
-                            <li>
-                                Red Dead Redemption 2
-                            </li>
-                            <li>
-                                The Simpsons Hit & Run
-                            </li>
-                            <li>
-                                Mario Strikers Charged
-                            </li>
-                            <li>
-                                Grand Theft Auto IV
-                            </li>
-                        </ul>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-
+            ))}
 
         </div>
     )
